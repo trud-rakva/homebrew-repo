439bd4d98b740681938e9258ee1d1c86768c531a
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2020-09-30 21:27:41 +0200

    Create libfido2.rb

---
+++ Formula/libfido2.rb
@@ -0,0 +1,48 @@
+class Libfido2 < Formula
+  desc "Provides library functionality for FIDO U2F & FIDO 2.0, including USB"
+  homepage "https://developers.yubico.com/libfido2/"
+  url "https://github.com/Yubico/libfido2/archive/1.5.0.tar.gz"
+  sha256 "5990f923c9390fe1e6a00ba5d1d1f74030e7344b855e971d9fb7223e70ff3122"
+  license "BSD-2-Clause"
+  revision 1
+
+  depends_on "cmake" => :build
+  depends_on "mandoc" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libcbor"
+  depends_on "libressl"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "man_symlink_html"
+      system "make", "man_symlink"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOF
+    #include <stddef.h>
+    #include <stdio.h>
+    #include <fido.h>
+    int main(void) {
+      fido_init(FIDO_DEBUG);
+      // Attempt to enumerate up to five FIDO/U2F devices. Five is an arbitrary number.
+      size_t max_devices = 5;
+      fido_dev_info_t *devlist;
+      if ((devlist = fido_dev_info_new(max_devices)) == NULL)
+        return 1;
+      size_t found_devices = 0;
+      int error;
+      if ((error = fido_dev_info_manifest(devlist, max_devices, &found_devices)) == FIDO_OK)
+        printf("FIDO/U2F devices found: %s\\n", found_devices ? "Some" : "None");
+      fido_dev_info_free(&devlist, max_devices);
+    }
+    EOF
+    system ENV.cc, "-std=c99", "test.c", "-I#{include}", "-I#{Formula["libressl"].include}", "-o", "test",
+                   "-L#{lib}", "-lfido2"
+    system "./test"
+  end
+end
