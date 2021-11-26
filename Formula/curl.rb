dcec078866bd39fc2f7f90018258807e6f0fa7e3
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2021-11-26 14:38:07 +0100

    Update curl.rb

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -44,7 +44,7 @@ class Curl < Formula
     ]
     # mk: remove openssl pkg-config to force libressl
     xpkgp = '$(echo $PKG_CONFIG_PATH | sed \'s/:[^:]*openssl[^:]*//\')'
-    system( { "PKG_CONFIG_PATH" => '$(echo $PKG_CONFIG_PATH | sed \'s/:[^:]*openssl[^:]*//\')' }, "echo", '$PKG_CONFIG_PATH' )
+    system( { "PKG_CONFIG_PATH" => 'AAAAA' }, "echo", '$PKG_CONFIG_PATH' )
     system( { "PKG_CONFIG_PATH" => '$(echo $PKG_CONFIG_PATH | sed \'s/:[^:]*openssl[^:]*//\')' }, "./configure", *args )
     system "make", "install"
   end
