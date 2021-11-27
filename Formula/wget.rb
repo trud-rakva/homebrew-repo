40c91f7e940fc842038e1ae555689f667b64a20f
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2021-11-27 11:41:24 +0100

    Create wget.rb

---
+++ Formula/wget.rb
@@ -0,0 +1,26 @@
+class Wget < Formula
+  desc "Internet file retriever"
+  homepage "https://www.gnu.org/software/wget/"
+  url "https://ftp.gnu.org/gnu/wget/wget-1.21.2.tar.gz"
+  sha256 "e6d4c76be82c676dd7e8c61a29b2ac8510ae108a810b5d1d18fc9a1d2c9a2497"
+
+  depends_on "pkg-config" => :build
+  depends_on "libidn2"
+  depends_on "trud-rakva/repo/libressl"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--with-ssl=libressl",
+                          "--with-libssl-prefix=#{Formula["libressl"].opt_prefix}",
+                          "--disable-pcre",
+                          "--disable-pcre2",
+                          "--without-libpsl",
+                          "--without-included-regex"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"wget", "-O", "/dev/null", "https://google.com"
+  end
+end
