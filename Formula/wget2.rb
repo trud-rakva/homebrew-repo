b2a94ba9d91a365949d2bb0245272941110786ba
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2022-01-05 18:03:19 +0100

    Update wget2.rb

--- Formula/wget2.rb
+++ Formula/wget2.rb
@@ -4,6 +4,7 @@ class Wget2 < Formula
   url "https://ftp.gnu.org/gnu/wget/wget2-2.0.0.tar.gz"
   sha256 "4fe2fba0abb653ecc1cc180bea7f04212c17e8fe05c85aaac8baeac4cd241544"
 
+  depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
