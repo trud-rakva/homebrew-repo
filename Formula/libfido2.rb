215d5d3371861ad21d65399f7ecde3a7f2ee734a
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2024-10-18 13:37:34 +0200

    Updated to  libfido2 1.15.0

--- Formula/libfido2.rb
+++ Formula/libfido2.rb
@@ -1,8 +1,8 @@
 class Libfido2 < Formula
   desc "Provides library functionality for FIDO U2F & FIDO 2.0, including USB"
   homepage "https://developers.yubico.com/libfido2/"
-  url "https://github.com/Yubico/libfido2/archive/refs/tags/1.14.0.tar.gz"
-  sha256 "3601792e320032d428002c4cce8499a4c7b803319051a25a0c9f1f138ffee45a"
+  url "https://github.com/Yubico/libfido2/archive/refs/tags/1.15.0.tar.gz"
+  sha256 "32e3e431cfe29b45f497300fdb7076971cb77fc584fcfa80084d823a6ed94fbb"
 
   depends_on "cmake" => :build
   depends_on "mandoc" => :build
@@ -10,10 +10,7 @@ class Libfido2 < Formula
   depends_on "libcbor"
   depends_on "trud-rakva/repo/libressl"
 
-  patch do
-    url "https://raw.githubusercontent.com/trud-rakva/homebrew-repo/master/Patch/libfido2-114-libressl-39-EVP_MD.patch"
-    sha256 "7555c8f7bc95996165d87374e49b619405ef5d23da2501bde7b25916d40d6848"
-  end
+  uses_from_macos "zlib"
   
   def install
     mkdir "build" do
