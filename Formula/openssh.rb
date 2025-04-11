f61decf72fcf1c8a91c6e463070c62c06ea0e718
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2025-04-11 16:55:08 +0200

    Updated to OpenSSH 10.0p1 (and removed old sb patches)

--- Formula/openssh.rb
+++ Formula/openssh.rb
@@ -21,32 +21,8 @@ class Openssh < Formula
   uses_from_macos "libedit"
   uses_from_macos "libxcrypt"
   uses_from_macos "zlib"
-  
-  resource "com.openssh.sshd.sb" do
-    url "https://raw.githubusercontent.com/apple-oss-distributions/OpenSSH/OpenSSH-268.100.4/com.openssh.sshd.sb"
-    sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
-  end
-
-  # Both these patches are applied by Apple.
-  # https://github.com/apple-oss-distributions/OpenSSH/blob/main/openssh/sandbox-darwin.c#L66
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a745f1fe726900974845d1b0dd3c3398d6/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
-    sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
-  end
-
-  # https://github.com/apple-oss-distributions/OpenSSH/blob/main/openssh/sshd.c#L532
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/aa6c71920318f97370d74f2303d6aea387fb68e4/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
-    sha256 "3f06fc03bcbbf3e6ba6360ef93edd2301f73efcd8069e516245aea7c4fb21279"
-  end
 
   def install
-    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
-
-    # Ensure sandbox profile prefix is correct.
-    # We introduce this issue with patching, it's not an upstream bug.
-    inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
-
     args = %W[
       --prefix=#{prefix}
       --sysconfdir=#{etc}/ssh
@@ -62,14 +38,6 @@ class Openssh < Formula
     system "make"
     ENV.deparallelize
     system "make", "install"
-
-    # This was removed by upstream with very little announcement and has
-    # potential to break scripts, so recreate it for now.
-    # Debian have done the same thing.
-    bin.install_symlink bin/"ssh" => "slogin"
-
-    buildpath.install resource("com.openssh.sshd.sb")
-    (etc/"ssh").install "com.openssh.sshd.sb" => "org.openssh.sshd.sb"
   end
 
   test do
