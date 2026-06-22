82df521c90782ce118eae77defb9f5fcfd582fee
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2026-06-22 18:12:01 +0200

    Formula fix

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -19,7 +19,6 @@ class Curl < Formula
 
   depends_on "brotli"
   depends_on "libnghttp2"
-  depends_on "libnghttp3"
   depends_on "libssh2"
   depends_on "zstd"
   depends_on "trud-rakva/repo/libressl"
@@ -40,7 +39,7 @@ class Curl < Formula
       --with-ca-fallback
       --with-secure-transport
       --with-libssh2
-      --with-nghttp3
+      --with-nghttp2
       --without-libpsl
       --with-apple-sectrust
       --with-gssapi
